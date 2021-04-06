6bd62363e10f88106b7d2c7830591c3364f37aa3
andrmiw9 <andreymiw@yahoo.com>
2021-04-07 01:18:36 +0300

    Рандомная генерация предметов

---
+++ Scenes/MainScene.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+onready var item = preload("res://Scenes/Item.tscn")
+var itemList = ["key"]
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	ItemGen()
+
+func ItemGen():
+	for	i in range (5):						# создаем 5 обьектов 
+		var new_item = item.instance()
+		$Items.add_child(new_item)
+		new_item.set_item(itemList[0])
+		new_item.position = Vector2(int(rand_range(0, 1000)), int(rand_range(0, 1000)))
+	
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
