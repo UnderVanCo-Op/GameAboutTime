d35a0b41cf1145d8bd0e44d7efc6c93362ccbc4d
andrmiw9 <andreymiw@yahoo.com>
2021-04-07 22:59:46 +0300

    баг устранен

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -10,11 +10,11 @@ onready var PlayerSprite = get_node("Sprite")
 func pick(item):
 	#itemsCount += 1
 	#print("Item count: ", str(itemsCount))
-	var it = item.get_name()
-	if it in inventory.keys():
-		inventory[it] += it.get_amount()
+	var itName = item.get_name()
+	if itName in inventory.keys():
+		inventory[itName] += item.get_amount()
 	else:
-		inventory[it] =  it.get_amount()
+		inventory[itName] =  item.get_amount()
 
 func _physics_process(_delta):
 	if Input.is_action_pressed("Player_goleft"):
