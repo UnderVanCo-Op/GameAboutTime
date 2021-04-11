7e29f91a64dec7186a34b9e68ff3dc07dec93728
andrmiw9 <andreymiw@yahoo.com>
2021-04-11 04:49:58 +0300

    2-ой ключ

--- Scripts/MainScene.gd
+++ Scripts/MainScene.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 onready var item = preload("res://Scenes/Item.tscn")
-var itemList = ["Key"]
+var itemList = ["Key", "Key2"]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,7 +11,7 @@ func ItemGen():
 	for	_i in range (5):						# создаем 5 обьектов 
 		var new_item = item.instance()
 		$Items.add_child(new_item)
-		new_item.set_item(itemList[0])
+		new_item.set_item(itemList[rand_range(0,2)])
 		randomize()
 		new_item.position = Vector2(int(rand_range(0, 1000)), int(rand_range(0, 1000)))
 	
