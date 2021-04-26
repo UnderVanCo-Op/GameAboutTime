3f4d1d3fc302dabe9393e96665aeb9cce447c601
andrmiw9 <andreymiw@yahoo.com>
2021-04-26 13:58:21 +0300

    фикс бага с полосами в настройках проекта, добавлена дверь и воспроизведение анимации у неё

---
+++ Scripts/Door.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+onready var ap = $AnimationPlayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Area2D_body_entered(body):
+	if body is KinematicBody2D:
+		ap.play("door_anim")
